cdcd43dc54b410232b6c00b4b260189e44ab8123
katrielaraujo <katrielaraujo@gmail.com>
2022-12-21 19:00:38 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::string;
+using std::cin;
+using std::cout;
+
+int main(int argc,char *argv){
+    std::ifstream file;
+
+    string filename;
+    cout << "File: ";
+
+    return 0;
+}
