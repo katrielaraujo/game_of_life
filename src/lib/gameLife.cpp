1e5d0204287a02808394cddd561a7995f8f50d49
katrielaraujo <katrielaraujo@gmail.com>
2022-12-22 16:24:20 -0300

    open_file para felipe

--- src/lib/gameLife.cpp
+++ src/lib/gameLife.cpp
@@ -23,24 +23,21 @@ namespace Life{
   }
 
   void GameLife::open_file(){
-    string line;
-    std::ifstream FileData(_filename);
-    if(FileData.is_open()){
-      FileData >> cell.n_rows >> cell.n_cols >> _birth;
-      while(not FileData.eof()){
-        for(auto ca: line){
-          for(int ii{0}; ii < cell.n_rows;++ii){
-            for(int jj{0};(jj < cell.n_cols) or (jj < int (line.size()));++jj)
-              cell.board.at(ii).at(jj) = (ca == _birth)? 1 : 0;
-          }
-          if(ca != _birth) 
-            _died = ca;
-        }
+    std::ifstream ifs{_filename};
+    std::string line;
+
+    if(ifs.is_open()){
+      ifs >> cell.n_rows >> cell.n_cols >> _birth;
+      while (std::getline(ifs, line)){
+
       }
+      _died = line[0];
+      ifs.close();
     }else{
       std::cerr << "Uh oh, "<<_filename<<" could not be opened for reading!\n";
       exit(EXIT_FAILURE);
     }
+
   }
 
   void GameLife::initialize(int argc,char **argv){
